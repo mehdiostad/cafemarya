219b2650e83b821abb6f4e8c3547424a77ae9158
mehdi <me.ostad80@gmail.com>
2024-03-30 23:37:52 +0330

    .env changed

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
  const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI);
   } catch (error) {
     console.log(error.message);
   }
