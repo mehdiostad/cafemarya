8d3e73acf2fc66b1a6bde48111fa12fbfd462d85
mehdi <me.ostad80@gmail.com>
2024-03-31 00:00:28 +0330

    ehsan

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
- const connect = async () => {
+const connect = async () => {
   try {
-    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI);
+    await mongoose.connect(
+      "mongodb+srv://meostad80:ZmvfaDp5LSB40bXK@cafemarya.e2xxom5.mongodb.net/cafeMarya"
+    );
   } catch (error) {
     console.log(error.message);
   }
